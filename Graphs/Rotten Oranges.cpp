3e52a0b8ff3012a7874364c136c2b575becfd3e9
ThisisAtharv <atharv.kurlapkar47@gmail.com>
2023-09-09 19:59:14 +0530

    'Rotten Oranges' modified.

--- Graphs/Rotten Oranges.cpp
+++ Graphs/Rotten Oranges.cpp
@@ -6,3 +6,56 @@
 // 3) Add a timer variable along with it in the queue and increment it for the newly added oranges in an iteration .
 // 4) Continue this until queue is not empty.
 
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> vis(n,vector<int> (m));
+
+        for(int i = 0; i < n;i++){
+            for(int  j =0; j < m;j++){
+                if(grid[i][j] == 2){
+                    q.push({{i,j}, 0});
+                    vis[i][j] = 2;
+                }else{
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        int tm = 0;
+        int drow[] ={-1,0,1,0};              // Checking the neighbours only in four directions
+        int dcol[] ={0,1,0,-1};
+
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+            tm = max(tm,t);
+            q.pop();
+
+            for(int  i= 0; i < 4;i++){
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0){
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol] = 2;
+                }
+            }
+        }
+
+        for(int i = 0; i < n;i++){
+            for(int j = 0; j < m;j++){
+                if(vis[i][j] != 2 && grid[i][j] == 1){
+                    return -1;
+                }
+            }
+        }
+
+        return tm;
+    }
+};
